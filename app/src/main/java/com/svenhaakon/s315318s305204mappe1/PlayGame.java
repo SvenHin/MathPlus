9acf5e497ab36235c992d5638ab6cc1e7f6a3a94
s305204 <s305204@stud.hioa.no>
2018-09-17 19:10:42 +0200

    Har merget lokalt, så commit push. Endringer i PlayGame og PlayGame XML

--- app/src/main/java/com/svenhaakon/s315318s305204mappe1/PlayGame.java
+++ app/src/main/java/com/svenhaakon/s315318s305204mappe1/PlayGame.java
@@ -1,8 +1,11 @@
 package com.svenhaakon.s315318s305204mappe1;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
@@ -13,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toolbar;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.security.PublicKey;
@@ -25,61 +29,102 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static com.svenhaakon.s315318s305204mappe1.R.array.answr_array;
 import static com.svenhaakon.s315318s305204mappe1.R.array.eq_array;
 
 public class PlayGame extends Activity {
 
-    Button answrBtn;
     List<String> questionArray;
     List<String> answerArray;
-    List<int[]> indexArray;
-
+    int[] indexArray;
     public int points;
-    public int arrayIterator = 0;
-
+    public int arrayIterator;
+    EditText inputText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playgame);
+
         Toolbar settingsToolbar = (Toolbar)findViewById(R.id.game_toolbar);
         setActionBar(settingsToolbar);
 
         //mTestArray = getResources().getStringArray(R.array.eq_array);
         questionArray =  Arrays.asList(getResources().getStringArray(R.array.eq_array));
         answerArray =  Arrays.asList(getResources().getStringArray(R.array.answr_array));
-        indexArray =   Arrays.asList(getResources().getIntArray(R.array.index_array));
 
-
+        Resources r = getResources();
+        indexArray = r.getIntArray(R.array.index_array);
 
         final Button button = findViewById(R.id.answrbtn);
+
+        inputText = (EditText) findViewById(R.id.eqInputBox);
+        initialize();
+
+
+
+
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                updateTextView();
+                checkAns();//sjekker og legger til poeng
+                nextQuestion();
 
             }
         });
     }
 
+    private void initialize(){
+
+        points = 0;
+        arrayIterator = 0;
+        shuffleArray(indexArray);
+        showQuestion();
 
 
-    private void shuffleList(ArrayList<String> t){
-        Random rnd = new Random();
-        rnd.setSeed(123);
-        Collections.shuffle(questionArray, rnd);
 
 
     }
 
-    private void updateTextView(){
-
-        TextView textView = (TextView)findViewById(R.id.EqDisplayBox);
-        textView.setText(questionArray.get(arrayIterator));
-        Log.d("qu", questionArray.get(arrayIterator));
-        Log.d("ans", answerArray.get(arrayIterator));
-        if (arrayIterator < questionArray.size() -1) arrayIterator ++;
-        else textView.setText("Ferdig!");
+    private static void shuffleArray(int[] array) {
+        int index;
+        Random random = new Random();
+        for (int i = array.length -1; i > 0; i--)
+        {
+            index = random.nextInt(i + 1);
+            if (index != i)
+            {
+                array[index] ^= array[i];
+                array[i] ^= array[index];
+                array[index] ^= array[i];
+            }
+        }
     }
+
+    private void showQuestion(){
+        textView = (TextView)findViewById(R.id.EqDisplayBox);
+        textView.setText(questionArray.get(indexArray[arrayIterator]));
+    }
+
+    private void checkAns(){
+        if((inputText.getText().toString().equals(answerArray.get(indexArray[arrayIterator]).toString()))){
+            points++;
+            Log.d("Points", points +"/15");
+        }
+
+    }
+
+    private void nextQuestion(){
+        arrayIterator++;
+        if (arrayIterator == questionArray.size()){textView.setText("Ferdig!");}
+        showQuestion();
+        inputText.setText("");
+    }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
